d1da7fcb647d8db75af9d8911ddff71b43977c66
Heni Urip <henni@pcr.ac.id>
2023-09-16 15:31:32 +0700

    soal1

--- Pertemuan 1/Soal 1.py
+++ Pertemuan 1/Soal 1.py
@@ -1,2 +1,13 @@
-print('tes')
-print('Heni älskar Nicko')
+jawab = 'ya'
+total = 0
+while(jawab.lower()=='ya'):
+    angka = int(input('masukkan angka dalam range 0-50: '))
+    if (0<=angka<=50):
+        total = total + angka
+    else:
+        print('angka ',angka, 'diluar batas range')
+    jawab = input('Apakah Anda masih ingin menambahkan angka lagi? ')
+if (total%2 == 0):
+    print('Jumlah keseluruhan angka yang Anda masukkan adalah ',total,' dan merupakan bilangan genap')
+else:
+    print('Jumlah keseluruhan angka yang Anda masukkan adalah ',total,' dan merupakan bilangan ganjil')
